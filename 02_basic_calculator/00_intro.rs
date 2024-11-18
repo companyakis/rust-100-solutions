9f33fdd42f0c5ab29520520d96ebc947e6a0e88e
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-18 13:51:49 +0300

    Create 00_intro.rs

---
+++ 02_basic_calculator/00_intro.rs
@@ -0,0 +1,29 @@
+// fn intro() -> &'static str {
+//     // TODO: fix me 👇
+//     "I'm ready to __!"
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use crate::intro;
+
+//     #[test]
+//     fn test_intro() {
+//         assert_eq!(intro(), "I'm ready to build a calculator in Rust!");
+//     }
+// }
+
+fn intro() -> &'static str {
+  // TODO: fix me 👇
+  "I'm ready to build a calculator in Rust!"
+}
+
+#[cfg(test)]
+mod tests {
+  use crate::intro;
+
+  #[test]
+  fn test_intro() {
+      assert_eq!(intro(), "I'm ready to build a calculator in Rust!");
+  }
+}
