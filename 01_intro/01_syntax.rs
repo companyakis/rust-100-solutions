113523bef546eb1b2f29d07648dd7cb58cda2259
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-18 13:48:42 +0300

    Create 01_syntax.rs

---
+++ 01_intro/01_syntax.rs
@@ -0,0 +1,30 @@
+// // TODO: fix the function signature below to make the tests pass.
+// // The input parameters should have the same type of the return type.
+// fn compute(a, b) -> u32 {
+//     // Don't touch the function body.
+//     a + b * 2
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use crate::compute;
+
+//     #[test]
+//     fn case() {
+//         assert_eq!(compute(1, 2), 5);
+//     }
+// }
+
+fn compute(a: u32, b: u32) -> u32 {
+  a + b * 2
+}
+
+#[cfg(test)]
+mod tests {
+  use crate::compute;
+
+  #[test]
+  fn case() {
+      assert_eq!(compute(1, 2), 5);
+  }
+}
